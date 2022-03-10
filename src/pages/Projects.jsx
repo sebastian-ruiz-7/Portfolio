94ed5056ee7a2e32023fa6fca8cb0a6044b8aa06
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2022-03-09 23:24:20 -0600

    [FIX] Se cambio la descripcion del phoneBook project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,7 +19,7 @@ export const Projects = () => {
 
         <ProjectCard title='Mariscos Pacífico app' link={'https://github.com/sebastian-ruiz-7/Mariscos-Pacifico'} image={mariscos} description={'This is a web app for a restaurant. You can take orders, update orders, get tickets, see the not yet delivered dishes. All in real time with Web Sockets, and more.'}/>
 
-        <ProjectCard title='PhoneBook' link={'https://github.com/sebastian-ruiz-7/PhoneBook'} image={phoneBook} description={'This is a site where you can see my last projects and get to know me a little better'}/>
+        <ProjectCard title='PhoneBook' link={'https://github.com/sebastian-ruiz-7/PhoneBook'} image={phoneBook} description={'This is a web app where you can handle your phone contacts through a digital phoneBook'}/>
 
         <ProjectCard title='My portfolio' link={'https://github.com/sebastian-ruiz-7/Portfolio'} image={portfolio} description={'This is a site where you can see my last projects and get to know me a little better'}/>
 
